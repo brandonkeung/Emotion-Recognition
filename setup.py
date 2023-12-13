fb5ae784f5cb5284d51d1c834f7209e217d3313a
blkeung@gmail.com <blkeung@gmail.com>
2023-12-13 00:25:04 -0800

    added logistic reg model and setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Emotion-Recognition',
+    version='1.0.0',
+    url='https://github.com/brandonkeung/Emotion-Recognition',
+    author='Brandon Keung',
+    author_email='brandonlkeung@gmail.com',
+    description='',
+    packages=find_packages(),    
+    install_requires=[
+        'Pillow',
+        'numpy',
+        'pandas',
+        'tqdm',
+        'os',
+        'scikit-learn',
+    ],
+)
